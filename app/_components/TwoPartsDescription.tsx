e802866cc39ec2c4b27508c217cb0d3c3cc8a9cd
barba <sandrolini.barbara@nebbialab.it>
2026-08-18 11:48:11 +0200

    Layout + info fixes pt.2

--- app/_components/TwoPartsDescription.tsx
+++ app/_components/TwoPartsDescription.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import {useState} from "react";
+import Markdown from "react-markdown";
 
 export default function TwoPartsDescription({partOne, partTwo, lang} : {partOne:string, partTwo:string, lang:string}) {
 
@@ -8,16 +9,21 @@ export default function TwoPartsDescription({partOne, partTwo, lang} : {partOne:
 
     return(
         <>
-            <p className="text-xl lato">
-                {partOne}
-            </p>
+            <div className="text-xl markdown lato">
+                <Markdown>
+                    {partOne}
+                </Markdown>
+            </div>
 
-            <p id="secondPart" className={`lato text-xl ${showPartTwo ? 'max-h-[1000px]' : 'max-h-0'} transition-all duration-500 overflow-hidden`}>
-                {partTwo}
-            </p>
+            <div id="secondPart"
+               className={`markdown lato text-xl ${showPartTwo ? 'max-h-[1000px]' : 'max-h-0'} transition-all duration-500 overflow-hidden`}>
+                <Markdown>
+                    {partTwo}
+                </Markdown>
+            </div>
 
             <div className="w-full text-end mt-2">
-                <button type="button"
+            <button type="button"
                         aria-controls="secondPart"
                         aria-expanded={showPartTwo}
                         // inert={!showPartTwo}

395f7a2cc6810e5be7efc93304d6b76638e9fb6f
barba <sandrolini.barbara@nebbialab.it>
2026-08-18 10:03:03 +0200

    Formatting fixes pt.2

--- app/_components/TicketCard.tsx
+++ app/_components/TicketCard.tsx
@@ -32,8 +32,8 @@ export default function TicketCard({el, layout, lang, disabled}: {el: TicketCard
                                 alt={el.immagine.alternativeText} width={300} height={200}/>
                             <div className="flex flex-col gap-2">
                                 {el.titolo
-                                    ? <h3 className="text-xl font-medium md:line-clamp-1">{el.nome}</h3>
-                                    : <h2 className="text-xl font-medium md:line-clamp-1">{el.nome}</h2>
+                                    ? <h3 className={`${layout === 'fourth' ? 'text-base' : layout === 'half' ? 'text-2xl py-1' : 'text-xl h-[56px]'} font-medium md:line-clamp-2`}>{el.nome}</h3>
+                                    : <h2 className={`${layout === 'fourth' ? 'text-base' : layout === 'half' ? 'text-2xl py-1' : 'text-xl h-[56px]'} font-medium md:line-clamp-2`}>{el.nome}</h2>
                                 }
                                 <p className={`${layout === 'fourth' ? 'text-sm' : ''} line-clamp-4 lato h-[96px]`}>{el.descrizione}</p>
                                 <div className="flex items-center justify-between mt-4">
